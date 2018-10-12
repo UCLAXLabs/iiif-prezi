3515d5565ceff930de3487188e3e16aa68cf7a75
broadwell <broadwell@diit-web-p02.it.ucla.edu>
2018-10-11 18:47:51 -0700

    Updates for HTTPS support

--- examples/build-from-directory.py
+++ examples/build-from-directory.py
@@ -2,23 +2,25 @@
 
 """
 
-from iiif_prezi.factory import ManifestFactory
-#from factory import ManifestFactory
+#from iiif_prezi.factory import ManifestFactory
+from factory import ManifestFactory
 import os
 
 # folder div for Cantaloupe server is | (urlencoded %7C)
-folder_div = '%7C'
-# folder_id = '/'
-base_dir = "/var/www/cantaloupe/images/"
+base_dir = "/usr/local/share/images/"
 # No trailing slash
-collection_id = "toganoo"
-images_subdir = "7"
+collection_id = "emel"
+images_subdir = ""
 if (images_subdir != ""):
   images_target = os.path.join(collection_id, images_subdir)
 else:
   images_target = collection_id
-images_uri = "http://164.67.17.127:8080/cantaloupe-4.0/iiif/2/"
-manifest_uri = "http://164.67.17.127/images/"
+  
+folder_div = '%7C'
+#folder_div = '/'
+
+images_uri = "https://marinus.library.ucla.edu/cantaloupe-4.0.1/iiif/2/"
+manifest_uri = "https://marinus.library.ucla.edu/iiif/"
 image_dir = base_dir + images_target
 manifest_label = images_target.replace("/", "_")
 prezi_dir = "."
@@ -26,18 +28,22 @@ prezi_dir = "."
 fac = ManifestFactory()
 fac.set_debug("error")
 fac.set_base_image_uri(images_uri + images_target.replace("/", folder_div) + folder_div)
+#fac.set_base_image_uri(images_uri + images_target.replace("/", folder_div) + folder_div)
 fac.set_base_image_dir(image_dir)
 fac.set_iiif_image_info()
 fac.set_base_prezi_uri(os.path.join(manifest_uri, collection_id))
+#fac.set_base_prezi_uri(manifest_uri)
 fac.set_base_prezi_dir(prezi_dir)
 
 mflbl = manifest_label.replace("_", " ").title()
 
-mfst = fac.manifest(label=mflbl, ident=manifest_label)
+#mfst = fac.manifest(label=mflbl, ident=manifest_label)
+mfst = fac.manifest(label=mflbl, ident="manifest")
 seq = mfst.sequence()
 for fn in sorted(os.listdir(image_dir)):
     #ident = fn[:-4]
     ident = fn
+    ident = fn.replace("+", "%2B")
     title = ident.replace("_", " ").title()
     cvs = seq.canvas(ident=ident, label=title)
     cvs.add_image_annotation(ident, True)
